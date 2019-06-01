28ed612bd5e3f15882bd9f6bd208cc5d6bf7e05f
LTitan <m15169917023@163.com>
2019-06-01 19:46:42 +0800

    更新最长公共子串

--- string/sequences.cpp
+++ string/sequences.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 //原来的开发者做的是0-9以内的数字算法，并且都是暴力算法
 //这里改进成动态规划算法
@@ -9,13 +9,11 @@ using namespace std;
 //最长公共子序列
 int max_subsequence_dp(string str1, string str2)
 {
-	int **dp = new int*[str1.length()+1];
-	for (int i = 0; i <= str1.length(); ++i)
-		dp[i] = new int[str2.length() + 1];
-
-	for (int i = 1; i <= str1.length(); ++i)
+	const int n =str1.length(), m = str2.length();
+	vector<vector<int> > dp(n+1, vector<int>(m+1,0));
+	for (int i = 1; i <= n; ++i)
 	{
-		for (int j = 1; j <= str2.length(); ++j)
+		for (int j = 1; j <= m; ++j)
 		{
 			if (str1[i - 1] == str2[j - 1])
 			{
@@ -25,16 +23,37 @@ int max_subsequence_dp(string str1, string str2)
 				dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
 		}
 	}
-	int temp = dp[str1.length()][str2.length()];
-	delete[] dp;
-	dp = nullptr;
-	return temp;
+	return dp[n][m];
 }
+//
+int maxSubstring(string str1,string str2)
+{
+	const int n =str1.length(), m = str2.length();
+	vector<vector<int> > dp(n+1, vector<int>(m+1,0));
+	int res = -1;
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= m; ++j)
+		{
+			if (str1[i - 1] == str2[j - 1])
+			{
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else
+				dp[i][j] = 0;
+			res = max(res, dp[i][j]);
+		}
+	}
+	return res;
+}
+
 int main(int argc, char const *argv[])
 {
 	string str1 = "abcdefghijk", str2 = "acdej";
 	//很明显是5
 	cout << max_subsequence_dp(str1, str2)<<endl;
-	system("pause");
+	
+	// cde
+	cout<<maxSubstring(str1,str2)<<endl;
 	return 0;
 }
